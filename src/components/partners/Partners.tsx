171194b6c409ce54a7ece11c23e342bd87b3926d
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-02-19 14:41:42 +0300

    hi

--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -1,6 +1,6 @@
 import "./Partners.scss";
 
-// import FloozImage from "../../assets/partners/flooz.png";
+// Importing image components for the partners
 import GCloudImage from "../../assets/partners/gcloud.png";
 import OKXImage from "../../assets/partners/okx.png";
 import AminoIcon from "../../assets/icons/Amino.png";
@@ -9,12 +9,14 @@ import GeckoTerminalIcon from "../../assets/icons/GeckoTerminal.webp";
 // import NvidiaIcon from "../../assets/icons/nvidia-logo.png";
 
 export default function Partners() {
-    return (
+   return (
         <section className="site-content-container partners-container">
+            {/* Heading for the partners section */}
             <h3>Partners</h3>
             <div className="partners">
                 <div className="images">
-                    <img className="aminiIcon" src={AminoIcon} alt="" />
+                    {/* Individual partner images */}
+                    <img className="aminoIcon" src={AminoIcon} alt="" />
                     <img src={GCloudImage} alt="" />
                     <img src={OKXImage} alt="" />
                     <img className="coincecko" src={CoinGeckoIcon} alt="" />
@@ -24,18 +26,17 @@ export default function Partners() {
                     {/* <img src={NvidiaIcon} alt="" /> */}
                     <img src={GeckoTerminalIcon} alt="" />
                     <div className="images3">
-                        <a href="https://chain.link/badge&quot" target="_blank">
-                            {" "}
+                        {/* Chainlink badges */}
+                        <a href="https://chain.link/badge" target="_blank">
                             <img
                                 src="https://uploads-ssl.webflow.com/5f6b7190899f41fb70882d08/632c50d9db40967f015be6d1_secured-with-chainlink-badge-market-data.svg"
                                 alt="market data secured with chainlink"
                             />
                         </a>
-                        <a href="https://chain.link/badge&quot" target="_blank">
-                            {" "}
+                        <a href="https://chain.link/badge" target="_blank">
                             <img
                                 src="https://uploads-ssl.webflow.com/5f6b7190899f41fb70882d08/632c50d91344fb274942c698_secured-with-chainlink-badge-randomness.svg"
-                                alt="market data secured with chainlink"
+                                alt="randomness secured with chainlink"
                             />
                         </a>
                     </div>
