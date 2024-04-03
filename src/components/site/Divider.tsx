b474b7bfaaa2d0d859dd7d1266efe4dade723820
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:30:48 +0300

    improve

--- src/components/site/Divider.tsx
+++ src/components/site/Divider.tsx
@@ -1,8 +1,23 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import "./Divider.scss";
 
-const Divider: FC = () => {
-    return <div className="site-divider" />;
-};
+interface DividerProps {
+  className?: string;
+  orientation?: "horizontal" | "vertical";
+}
+
+const Divider: FC<DividerProps> = forwardRef((props, ref) => {
+  const { className, orientation = "horizontal", ...rest } = props;
+
+  return (
+    <div
+      ref={ref}
+      className={`site-divider ${orientation} ${className}`}
+      role={orientation === "horizontal" ? "separator" : undefined}
+      aria-orientation={orientation}
+      {...rest}
+    />
+  );
+});
 
 export default Divider;
