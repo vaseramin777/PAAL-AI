667cad6e704e8f3545c303c24b982c6929aa2d31
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:30:55 +0300

    improve

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import Home from "./routes/Home";
 import "./styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+try {
+  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+  root.render(
     <React.StrictMode>
-        <Home />
+      <title>My App</title>
+      <Home />
     </React.StrictMode>
-);
+  );
+} catch (err) {
+  console.error("Error rendering the app", err);
+}
