6481742d0859c27f60430d41dc6cf1569a0b6a6f
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:30:59 +0300

    improve

--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -12,36 +12,43 @@ export default function Partners() {
    return (
         <section className="site-content-container partners-container">
             {/* Heading for the partners section */}
-            <h3>Partners</h3>
+            <h2>Partners</h2>
             <div className="partners">
                 <div className="images">
                     {/* Individual partner images */}
-                    <img className="aminoIcon" src={AminoIcon} alt="" />
-                    <img src={GCloudImage} alt="" />
-                    <img src={OKXImage} alt="" />
-                    <img className="coincecko" src={CoinGeckoIcon} alt="" />
+                    <figure className="aminoIcon">
+                        <img className="image" src={AminoIcon} alt="Amino logo" />
+                        <figcaption className="figure-caption">Amino</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="image" src={GCloudImage} alt="Google Cloud logo" />
+                        <figcaption className="figure-caption">Google Cloud</figcaption>
+                    </figure>
+                    <figure>
+                        <img className="image" src={OKXImage} alt="OKX logo" />
+                        <figcaption className="figure-caption">OKX</figcaption>
+                    </figure>
+                    <figure className="coincecko">
+                        <img className="image" src={CoinGeckoIcon} alt="CoinGecko logo" />
+                        <figcaption className="figure-caption">CoinGecko</figcaption>
+                    </figure>
                 </div>
                 <div className="images2">
                     {/* <img src={CoinGeckoIcon} alt="" /> */}
                     {/* <img src={NvidiaIcon} alt="" /> */}
-                    <img src={GeckoTerminalIcon} alt="" />
-                    <div className="images3">
+                    <figure>
+                        <img className="image" src={GeckoTerminalIcon} alt="GeckoTerminal logo" />
+                        <figcaption className="figure-caption">GeckoTerminal</figcaption>
+                    </figure>
+                    <div className="images3 chainlink-badges">
                         {/* Chainlink badges */}
-                        <a href="https://chain.link/badge" target="_blank">
+                        <a
+                            href="https://chain.link/badge"
+                            target="_blank"
+                            aria-label="market data secured with chainlink"
+                        >
                             <img
+                                className="image"
                                 src="https://uploads-ssl.webflow.com/5f6b7190899f41fb70882d08/632c50d9db40967f015be6d1_secured-with-chainlink-badge-market-data.svg"
                                 alt="market data secured with chainlink"
-                            />
-                        </a>
-                        <a href="https://chain.link/badge" target="_blank">
-                            <img
-                                src="https://uploads-ssl.webflow.com/5f6b7190899f41fb70882d08/632c50d91344fb274942c698_secured-with-chainlink-badge-randomness.svg"
-                                alt="randomness secured with chainlink"
-                            />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+
