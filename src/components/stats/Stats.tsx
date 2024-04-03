39622caa23360d4955ebe0483af528c0afee86f6
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:31:03 +0300

    improve

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -3,92 +3,93 @@ import "./Stats.scss";
 import { useInView } from "react-intersection-observer";
 
 const Stats: FC = () => {
-    const statItems = [
-        {
-            number: 1350,
-            sign: "+",
-            label: "Active Groups",
-        },
-        {
-            number: 1,
-            sign: ".2M+",
-            label: "Members",
-        },
-        {
-            number: 12500,
-            sign: "+",
-            label: "Users",
-        },
-        {
-            number: 50000,
-            sign: "+",
-            label: "Prompts",
-        },
-    ];
+  const statItems = [
+    {
+      number: 1350,
+      sign: "+",
+      label: "Active Groups",
+    },
+    {
+      number: 1200000,
+      sign: ".2M+",
+      label: "Members",
+    },
+    {
+      number: 12500,
+      sign: "+",
+      label: "Users",
+    },
+    {
+      number: 50000,
+      sign: "+",
+      label: "Prompts",
+    },
+  ];
 
-    return (
-        <section className="stats-container site-content-container">
-            <div className="stats-row">
-                {statItems.map(({ label, sign, number }, index) => (
-                    <StatItem
-                        key={index}
-                        label={label}
-                        sign={sign}
-                        number={number}
-                        duration={300}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="stats-container site-content-container">
+      <div className="stats-row">
+        {statItems.map(({ label, sign, number }, index) => (
+          <StatItem
+            key={index}
+            label={label}
+            sign={sign}
+            number={number}
+            duration={300}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
 
 type StatItemProps = {
-    number: number;
-    sign: string;
-    label: string;
-    duration: number;
+  number: number;
+  sign: string;
+  label: string;
+  duration: number;
 };
 
 const StatItem: FC<StatItemProps> = ({ number, sign, label, duration }) => {
-    const { ref, inView } = useInView({
-        threshold: 0,
-    });
-    const [shown, setShown] = useState(0);
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  const [shown, setShown] = useState(0);
 
-    useEffect(() => {
-        if (inView) {
-            if (shown < number) {
-                const startTime = Date.now();
-                const interval = setInterval(() => {
-                    const elapsedTime = Date.now() - startTime;
-                    if (elapsedTime < duration) {
-                        const progress =
-                            (elapsedTime / duration) * (number - shown);
-                        setShown(Math.ceil(shown + progress));
-                    } else {
-                        setShown(number);
-                        clearInterval(interval); // Stop the interval when the duration is reached
-                    }
-                }, 16); // Using a small interval for smoother animation
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
 
-                return () => clearInterval(interval);
-            }
-        } else {
-            setShown(0);
-        }
-    }, [number, inView, shown, duration]);
+    if (inView) {
+      if (shown < number) {
+        const startTime = Date.now();
+        interval = setInterval(() => {
+          const elapsedTime = Date.now() - startTime;
+          if (elapsedTime < duration) {
+            const progress = (elapsedTime / duration) * (number - shown);
+            setShown(Math.ceil(shown + progress));
+          } else {
+            setShown(number);
+            clearInterval(interval); // Stop the interval when the duration is reached
+          }
+        }, 16); // Using a small interval for smoother animation
+      }
+    } else {
+      setShown(0);
+    }
 
-    return (
-        <div className="stat-item" ref={ref}>
-            <span className="stat-number">
-                {shown}
-                {sign}
-            </span>
+    return () => clearInterval(interval);
+  }, [number, inView, shown, duration]);
 
-            <span className="stat-label">{label}</span>
-        </div>
-    );
+  return (
+    <div className="stat-item" ref={ref}>
+      <span className="stat-number">
+        {shown}
+        {sign}
+      </span>
+
+      <span className="stat-label">{label}</span>
+    </div>
+  );
 };
