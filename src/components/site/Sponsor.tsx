8bcd982c99df4013313e1c3f31bfecb6a7d8d9b9
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:31:12 +0300

    improve

--- src/components/site/Sponsor.tsx
+++ src/components/site/Sponsor.tsx
@@ -11,23 +11,29 @@ type SponsorProps = {
 const Sponsor: FC<SponsorProps> = ({ mobileCenter }) => {
     return (
         <div className={`sponsor ${mobileCenter ? "mobile-sponsor" : ""}`}>
-            <span className="sponsor-title">Supported by</span>
+            <span className="sponsor-title" aria-label="Supported by">
+                Supported by
+            </span>
             <img
                 className="sponsor-icon"
                 src={GoogleCloudLogo}
                 alt="Google Cloud Platform"
+                height={50}
+                width={150}
+                role="img"
+                loading="lazy"
             />
 
-            <a className="sponsor-icon" href="https://www.coingecko.com">
-                {" "}
+            <a
+                className="sponsor-icon"
+                href="https://www.coingecko.com"
+                rel="noopener noreferrer"
+                title="Coin Gecko"
+            >
                 <img
                     className="sponsor-icon"
                     src={CoinGecko}
                     alt="Coin Gecko"
-                />
-            </a>
-        </div>
-    );
-};
-
-export default Sponsor;
+                    key={CoinGecko}
+                    height={50}
+                   
