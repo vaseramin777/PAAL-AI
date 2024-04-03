efb5ac85693792c1617c3c017af319402daa83a3
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:30:43 +0300

    improve

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,29 @@
 module.exports = {
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": "warn",
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended", // added for better React support
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json", // added to enable type-checking
+  },
+  plugins: ["react-refresh", "react"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": "off", // disable this rule if you're using functional components
+  },
+  settings: {
+    react: {
+      version: "detect", // detect the version of React automatically
     },
+  },
 };
