ccc5bfecdac00d647934eabf80e9f2a5afa10249
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:31:06 +0300

    improve

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Define a type for the Vite configuration
+interface IViteConfig {
+  plugins: react[]
+}
+
+// Export a default object that conforms to the IViteConfig type
 export default defineConfig({
   plugins: [react()],
-})
+} as IViteConfig)
