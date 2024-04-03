175b821aceeb95daeb6a6659eac23d93e898e5c5
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-04-03 23:30:53 +0300

    improve

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -2,29 +2,13 @@ import { FC } from "react";
 import "./Footer.scss";
 
 import PAALAILogo from "../../assets/site/paal-ai-logo.png";
-// import Button from "./Button";
 import Socials from "./Socials";
+import Button from "./Button";
 
 const Footer: FC = () => {
-    return (
-        <footer className="site-footer site-content-container">
-            <img src={PAALAILogo} alt="PAAL AI" className="company-logo" />
-            <div className="footer-logo">
-                <a href="mailto:admin@paalai.io">admin@paalai.io</a>
-                <Socials small />
-            </div>
+  return (
+    <footer className="site-footer site-content-container">
+      <img
+        src={PAALAILogo}
+        alt="PAAL AI"
 
-            {/* <div className="actions">
-                <Button
-                    label="Whitepaper"
-                    mini
-                    link="https://paalai.io/whitepaper.pdf"
-                    className="footer-whitepaper"
-                />
-
-            </div> */}
-        </footer>
-    );
-};
-
-export default Footer;
